86d48fe57ef71637c7b99fa57d9a638657a7e040
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-07-19 23:17:54 +0200

    Implement cocoa_makeWindow, cocoa_startRunLoop and cocoa_stopRunLoop in GSTCocoa

--- gst-cocoa.h
+++ gst-cocoa.h
@@ -1,6 +1,10 @@
 #ifndef GST_COCOA_H
 #define GST_COCOA_H 1
 
+void cocoa_start();
 void cocoa_output(char* text);
+void cocoa_makeWindow(int width, int height, char* title);
+void cocoa_startRunLoop();
+void cocoa_stopRunLoop();
 
 #endif
--- gst-cocoa.m
+++ gst-cocoa.m
@@ -1,5 +1,59 @@
 #include "gst-cocoa.h"
+#include <AppKit/AppKit.h>
+#include <Foundation/Foundation.h>
 #include <stdio.h>
+
+@interface GSTCocoaAppDelegate : NSObject <NSApplicationDelegate>
+@end
+
+@implementation GSTCocoaAppDelegate
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
+    return YES;
+}
+
+@end
+
+static GSTCocoaAppDelegate *appDelegate;
+static NSAutoreleasePool *autoreleasePool;
+
+void cocoa_start() {
+    // Create a memory pool that will be drained upon release.
+    autoreleasePool = [[NSAutoreleasePool alloc] init];
+    
+    // Create the shared application instance backing NSApp.
+    [NSApplication sharedApplication];
+
+    appDelegate = [[GSTCocoaAppDelegate alloc] init];
+    [NSApp finishLaunching];
+    [NSApp setDelegate:appDelegate];
+}
+
+void cocoa_startRunLoop() {
+    [NSApp run];
+    [autoreleasePool drain];
+}
+
+void cocoa_stopRunLoop() {
+    [NSApp stop:NSApp];
+}
+
+void cocoa_makeWindow(int width, int height, char *title) {
+    NSUInteger windowStyle = NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable;
+    NSRect windowRect = NSMakeRect(0, 0, width, height);
+    NSWindow * window = [[NSWindow alloc] initWithContentRect:windowRect
+                                          styleMask:windowStyle
+                                          backing:NSBackingStoreBuffered
+                                          defer:NO];
+    [window setTitle:[NSString stringWithCString:title encoding:NSUTF8StringEncoding]];
+    [window autorelease];
+
+    NSWindowController * windowController = [[NSWindowController alloc] initWithWindow:window];
+    [windowController autorelease];
+
+    [window makeKeyWindow];
+    [window orderFrontRegardless];
+}
 
 void cocoa_output(char *text) {
     printf("Output: %s\n", text);
